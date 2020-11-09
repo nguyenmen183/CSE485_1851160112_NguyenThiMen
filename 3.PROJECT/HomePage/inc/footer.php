a1ccdefc31695cec94d79015cc7ed90c787522f1
dtphuong1012 <67354366+dtphuong1012@users.noreply.github.com>
2020-11-09 13:15:20 +0700

    them vao mo hinh mvc

---
+++ 3.PROJECT/HomePage/inc/footer.php
@@ -0,0 +1,51 @@
+<footer class="bg-secondary mt-5 ">
+      
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <h2 class="text-light">Liên hệ</h2>
+          <hr>
+          <div >
+            <p class="text-light">CodePower.vn (phiên bản thử nghiệm)</p>
+            <p class="text-light"><i class="fa fa-envelope-o text-light"></i>&nbsp; E-mail: <a style="color: #82b74b;"
+                href="mailto:support@codepower.vn">support@codepower.vn</a></p>
+            <p style="margin-top:24px;"><img src="images/PB_AWS_logo_RGB.png" alt="Powered by AWS" title="Powered by AWS"
+                style="background-color: #fff;padding: 10px;max-width:80%;"></p>
+          </div>
+        </div>
+    
+      </div>
+      <div class=" bg-dark ">
+        <div class="row">
+        
+          <div class=" text-light">
+            <span>© 2018 CodePower.vn</span>
+            <div class=" pull-right">
+              <a class=" fa fa-facebook" href="https://facebook.com/codepower.vn" target="_blank" rel="noreferrer">
+              </a>
+            </div>
+          </div>
+        </div>
+    
+    
+    
+      </div>
+    
+    
+    
+    </footer>
+
+
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-chevron-circle-up"></i></button>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+  </body>
+</html>
